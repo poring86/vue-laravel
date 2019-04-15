62822125e7819580e3cedcfbffe642816c5065e1
Matheus <matheuslino86a@gmail.com>
2019-04-14 20:56:50 -0400

    inicio

--- app/Task.php
+++ app/Task.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['title', 'uuid'];
+    protected $fillable = ['title', 'id', 'uuid'];
 
     public function task(){
         return $this->hasMany(Task::class);
